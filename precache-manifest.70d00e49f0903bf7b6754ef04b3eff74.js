97200d6458555b534b8ca05e5681f3c410aab2c3
himanshi.n97 <himanshi.n97@gmail.com>
2020-10-01 16:32:07 +0530

    Updates

---
+++ precache-manifest.70d00e49f0903bf7b6754ef04b3eff74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e6a1a18638797df6806791063593b99",
+    "url": "/shopping_cart_react/index.html"
+  },
+  {
+    "revision": "8e77ef48396b8b443145",
+    "url": "/shopping_cart_react/static/css/main.15c2a328.chunk.css"
+  },
+  {
+    "revision": "b4d720c20c4184b3b337",
+    "url": "/shopping_cart_react/static/js/2.2ca13306.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping_cart_react/static/js/2.2ca13306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e77ef48396b8b443145",
+    "url": "/shopping_cart_react/static/js/main.c56759b7.chunk.js"
+  },
+  {
+    "revision": "65686d6b6a02a651f915",
+    "url": "/shopping_cart_react/static/js/runtime-main.22828521.js"
+  }
+]);
