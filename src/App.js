a0b538430883b81230ac803cf785cf373104a4e2
himanshi.n97 <himanshi.n97@gmail.com>
2020-10-01 16:17:13 +0530

    Test React Shopping App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,105 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Cart from './components/Cart';
+import Filter from './components/Filter';
+import Products from './components/Products';
+import data from './data.json';
 
-function App() {
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      products: data.products,
+      cartItems: [],
+      size:"",
+      sort:"",
+    };
+  }
+
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({ cartItems: cartItems.filter(x => x._id !== product._id)})
+    ;
+  }
+  addToCart =(product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach(item => {
+      if(item._id === product._id){
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if(!alreadyInCart){
+      cartItems.push({...product, count: 1});
+    }
+    this.setState({cartItems});
+  }
+  sortProducts = (event) => {
+    //implement
+    const sort = event.target.value;
+    console.log(event.target.value);
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products.slice().sort((a,b) => 
+        sort ==="lowest"?
+        a.price > b.price ? 1:-1:
+        sort ==="highest"?
+        a.price < b.price ? 1:-1:
+        a._id > b._id ? 1:-1
+
+      )
+    }));
+  }
+
+  filterProducts = (event) => {
+    //implement
+    console.log(event.target.value);
+    if(event.target.value === ""){
+      this.setState({size: event.target.value, product:data.products});
+    } else{
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(product => product.availableSizes.indexOf(event.target.value)>=0
+        ),
+      });
+    }
+    
+  };
+
+  render(){
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+     
+    <div className = "grid-container"> 
+      <header>
+        <a href="/">React Shopping Cart</a>
       </header>
+      <main>
+        <div className="content">
+          <div className="main">
+            <Filter count={this.state.products.length}
+            size={this.state.size}
+            sort={this.state.sort}
+            filterProducts={this.filterProducts}
+            sortProducts={this.sortProducts}
+            ></Filter>
+            <Products products={this.state.products} addToCart={this.addToCart}></Products>
+          </div>
+          <div className="sidebar">
+            <Cart cartItems={this.state.cartItems}
+             removeFromCart={this.removeFromCart}/>
+          </div>
+        </div>
+      </main>
+      <footer>
+        Copy right 2020
+      </footer>
+
+    </div>
     </div>
   );
+  }
 }
 
 export default App;
